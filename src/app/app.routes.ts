6d3dfe756650b9bff325635bd3218944bd9a1af8
Dener-Fernandes <dener.fernandes.oliveira@gmail.com>
2026-05-01 18:40:12 -0400

    feat: add login screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,11 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/movies/movies').then((m) => m.Movies),
   },
   {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register').then((m) => m.Register),
+  },
+  {
     path: '**',
     redirectTo: '',
   },
---
+++ src/app/pages/register/register.ts
@@ -0,0 +1,65 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  imports: [ReactiveFormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class Register {
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+
+  form: FormGroup = this.fb.group(
+    {
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required],
+    },
+    { validators: this.passwordMatchValidator },
+  );
+
+  passwordMatchValidator(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+
+  get name() {
+    return this.form.get('name');
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+
+  get confirmPassword() {
+    return this.form.get('confirmPassword');
+  }
+
+  onSubmit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    } else {
+      console.log(this.form.value);
+    }
+  }
+
+  goToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+}
